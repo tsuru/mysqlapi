814efcab22b3b18ccab7c59b8c7b9f8d45364a40
Andrews Medina <andrewsmedina@gmail.com>
2012-06-20 17:59:02 -0300

    method for drop database

--- mysqlapi/api/models.py
+++ mysqlapi/api/models.py
@@ -6,3 +6,7 @@ class DatabaseManager(object):
     def create(self, name):
         cursor = connection.cursor()
         cursor.execute("CREATE DATABASE %s default character set utf8 default collate utf8_general_ci" % name)
+
+    def drop(self, name):
+        cursor = connection.cursor()
+        cursor.execute("DROP DATABASE %s" % name)
--- mysqlapi/api/tests.py
+++ mysqlapi/api/tests.py
@@ -12,7 +12,15 @@ class DatabaseTestCase(TestCase):
     def test_create(self):
         db = DatabaseManager()
         db.create(name="newdatabase")
-
         self.cursor.execute("select SCHEMA_NAME from information_schema.SCHEMATA where SCHEMA_NAME = 'newdatabase'")
         row = self.cursor.fetchone()
         self.assertEqual("newdatabase", row[0])
+        db.drop(name="newdatabase")
+
+    def test_drop(self):
+        db = DatabaseManager()
+        db.create(name="otherdatabase")
+        db.drop(name="otherdatabase")
+        self.cursor.execute("select SCHEMA_NAME from information_schema.SCHEMATA where SCHEMA_NAME = 'otherdatabase'")
+        row = self.cursor.fetchone()
+        self.assertFalse(row)
