39a2fdba0f6fb444e70b8c07c7d62a043ea14f29
Andrews Medina <andrewsmedina@gmail.com>
2012-06-21 17:53:07 -0300

    fixed url for drop database

--- mysqlapi/settings.py
+++ mysqlapi/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mysqlapi',                      # Or path to database file if using sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
--- mysqlapi/urls.py
+++ mysqlapi/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^resources/$', 'mysqlapi.api.views.create'),
-    url(r'^resources/(?P<appname>[\w-]+)/$', 'mysqlapi.api.views.destroy'),
+    url(r'^resources/(?P<appname>[\w-]+)/$', 'mysqlapi.api.views.drop'),
 )
