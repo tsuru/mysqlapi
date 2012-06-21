d2503e725f14ee81af482e616409f63a22a2687b
Andrews Medina <andrewsmedina@gmail.com>
2012-06-21 12:03:32 -0300

    views for create and drop service instance

--- mysqlapi/api/tests.py
+++ mysqlapi/api/tests.py
@@ -1,7 +1,50 @@
 from django.test import TestCase
 from django.db import connection
+from django.test.client import RequestFactory
 
 from mysqlapi.api.models import DatabaseManager
+from mysqlapi.api.views import create, drop
+
+
+class DatabaseViewTestCase(TestCase):
+
+    def setUp(self):
+        self.cursor = connection.cursor()
+
+    def test_create(self):
+        request = RequestFactory().post("/", {"appname": "ciclops"})
+        response = create(request)
+        self.assertEqual(201, response.status_code)
+
+        self.cursor.execute("select SCHEMA_NAME from information_schema.SCHEMATA where SCHEMA_NAME = 'ciclops'")
+        row = self.cursor.fetchone()
+        self.assertEqual("ciclops", row[0])
+
+        self.cursor.execute("select User, Host FROM mysql.user WHERE User='ciclops' AND Host='localhost'")
+        row = self.cursor.fetchone()
+        self.assertEqual("ciclops", row[0])
+        self.assertEqual("localhost", row[1])
+
+        db = DatabaseManager()
+        db.drop_user(username="ciclops", host="localhost")
+        db.drop(name="ciclops")
+
+    def test_drop(self):
+        db = DatabaseManager()
+        db.create(name="ciclops")
+        db.create_user(username="ciclops", host="localhost")
+
+        request = RequestFactory().delete("/ciclops")
+        response = drop(request, "ciclops")
+        self.assertEqual(200, response.status_code)
+
+        self.cursor.execute("select SCHEMA_NAME from information_schema.SCHEMATA where SCHEMA_NAME = 'ciclops'")
+        row = self.cursor.fetchone()
+        self.assertFalse(row)
+
+        self.cursor.execute("select User, Host FROM mysql.user WHERE User='ciclops' AND Host='localhost'")
+        row = self.cursor.fetchone()
+        self.assertFalse(row)
 
 
 class DatabaseTestCase(TestCase):
--- mysqlapi/api/views.py
+++ mysqlapi/api/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+from mysqlapi.api.models import DatabaseManager
+
+
+def create(request):
+    db = DatabaseManager()
+    db.create(request.POST["appname"])
+    db.create_user(request.POST["appname"], "localhost")
+    return HttpResponse("", status=201)
+
+
+def drop(request, appname):
+    db = DatabaseManager()
+    db.drop(appname)
+    db.drop_user(appname, "localhost")
+    return HttpResponse("", status=200)
