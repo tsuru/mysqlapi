fb8f5f0dce700ba05c1198a80ab86ab0b232d73b
Andrews Medina <andrewsmedina@gmail.com>
2012-06-22 18:27:45 -0300

    refactoring database model

--- mysqlapi/api/models.py
+++ mysqlapi/api/models.py
@@ -14,6 +14,7 @@ class DatabaseManager(object):
         self.host = "localhost"
         self.port = "3306"
         self._password = None
+        self.cursor = connection.cursor()
 
     @property
     def password(self):
@@ -22,17 +23,13 @@ class DatabaseManager(object):
         return self._password
 
     def create(self):
-        cursor = connection.cursor()
-        cursor.execute("CREATE DATABASE %s default character set utf8 default collate utf8_general_ci" % self.name)
+        self.cursor.execute("CREATE DATABASE %s default character set utf8 default collate utf8_general_ci" % self.name)
 
     def drop(self):
-        cursor = connection.cursor()
-        cursor.execute("DROP DATABASE %s" % self.name)
+        self.cursor.execute("DROP DATABASE %s" % self.name)
 
     def create_user(self):
-        cursor = connection.cursor()
-        cursor.execute("grant all privileges on %s.* to %s@%s identified by '%s'" % (self.name, self.name, self.host, self.password))
+        self.cursor.execute("grant all privileges on %s.* to %s@%s identified by '%s'" % (self.name, self.name, self.host, self.password))
 
     def drop_user(self):
-        cursor = connection.cursor()
-        cursor.execute("drop user %s@%s" % (self.name, self.host))
+        self.cursor.execute("drop user %s@%s" % (self.name, self.host))
