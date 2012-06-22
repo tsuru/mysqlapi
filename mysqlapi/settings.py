ad761cc4514c123e1cba3b7a85164444c9a09bf7
Andrews Medina <andrewsmedina@gmail.com>
2012-06-22 16:43:56 -0300

    removing log comments

--- mysqlapi/settings.py
+++ mysqlapi/settings.py
@@ -116,11 +116,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
