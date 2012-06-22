b42e40cf64c2c0f6fc4c226a94e355e0aae885b8
Andrews Medina <andrewsmedina@gmail.com>
2012-06-22 16:09:10 -0300

    removing some comment

--- mysqlapi/settings.py
+++ mysqlapi/settings.py
@@ -1,12 +1,9 @@
-# Django settings for mysqlapi project.
 import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
+ADMINS = ()
 
 MANAGERS = ADMINS
 
