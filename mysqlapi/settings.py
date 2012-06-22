3ba2b68168e5c87ecca8767e7d98941989c94a6b
Andrews Medina <andrewsmedina@gmail.com>
2012-06-22 15:53:56 -0300

    getting HOST env

--- mysqlapi/settings.py
+++ mysqlapi/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': os.environ.GET('MYSQLAPI_HOST', 'localhost'),                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': os.environ.get('MYSQLAPI_HOST', 'localhost'),                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
