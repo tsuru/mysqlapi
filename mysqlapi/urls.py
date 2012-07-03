4e9799cc3fe189dc5833d4a7007d8e6389fdd84d
Francisco Souza <f@souza.cc>
2012-07-03 14:07:29 -0300

    urls: pep8 fixes

--- mysqlapi/urls.py
+++ mysqlapi/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('',
-    url(r'^resources/$', 'mysqlapi.api.views.create_database'), #post
-    url(r'^resources/(?P<name>[\w-]+)/$', 'mysqlapi.api.views.create_user_or_drop_database'), #post and delete
-    url(r'^resources/(?P<name>[\w-]+)/export/$', 'mysqlapi.api.views.export'), #get
-    url(r'^resources/(?P<name>[\w-]+)/hostname/(?P<hostname>[\w.]+)/$', 'mysqlapi.api.views.drop_user'), #delete
+    url(r'^resources/$', 'mysqlapi.api.views.create_database'),  # post
+    url(r'^resources/(?P<name>[\w-]+)/$', 'mysqlapi.api.views.create_user_or_drop_database'),  # post and delete
+    url(r'^resources/(?P<name>[\w-]+)/export/$', 'mysqlapi.api.views.export'),  # get
+    url(r'^resources/(?P<name>[\w-]+)/hostname/(?P<hostname>[\w.]+)/$', 'mysqlapi.api.views.drop_user'),  # delete
 )
