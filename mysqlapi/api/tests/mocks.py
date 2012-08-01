2efa0d710f2439e54ed6858a2403e85d5a03ff7e
Francisco Souza <f@souza.cc>
2012-08-01 13:43:09 -0300

    api/tests: added mock for multiple failures before success

--- mysqlapi/api/tests/mocks.py
+++ mysqlapi/api/tests/mocks.py
@@ -5,8 +5,25 @@ class FakeEC2Client(object):
 
     def run(self, instance):
         self.actions.append("run instance %s" % instance.name)
+        instance.ec2_id = "i-029"
         return True
 
     def get(self, instance):
         self.actions.append("get instance %s" % instance.name)
+        instance.host = "127.0.0.1"
+        instance.state = "running"
         return True
+
+
+class MultipleFailureEC2Client(FakeEC2Client):
+
+    def __init__(self, times, *args, **kwargs):
+        self.times = times
+        self.failures = 0
+        super(MultipleFailureEC2Client, self).__init__(*args, **kwargs)
+
+    def get(self, instance):
+        if self.failures < self.times:
+            self.failures += 1
+            return False
+        return super(MultipleFailureEC2Client, self).get(instance)
