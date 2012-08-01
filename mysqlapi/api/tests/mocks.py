ba3823345698ea6832b4d6824546f049380fece9
Francisco Souza <f@souza.cc>
2012-08-01 13:27:51 -0300

    api/tests/mocks: added get method to FakeEC2Client

--- mysqlapi/api/tests/mocks.py
+++ mysqlapi/api/tests/mocks.py
@@ -6,3 +6,7 @@ class FakeEC2Client(object):
     def run(self, instance):
         self.actions.append("run instance %s" % instance.name)
         return True
+
+    def get(self, instance):
+        self.actions.append("get instance %s" % instance.name)
+        return True
