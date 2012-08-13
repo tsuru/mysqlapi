207ff6e502e231a8eabb0b042d556b23cecc7b89
Francisco Souza <f@souza.cc>
2012-08-13 17:31:09 -0300

    wsgi: using application instead of app

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysqlapi.settings")
 
 from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()
+application = get_wsgi_application()
