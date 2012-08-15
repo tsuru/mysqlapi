4f823367adb3771662cb4b13538cb5c67cd7b811
Francisco Souza <f@souza.cc>
2012-08-14 21:34:28 -0300

    wsgi: move databasecreator instantiation to wsgi.py
    Django sucks at this, I think we should use something else...
    
    Closes #13.

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,32 @@
+# -*- coding: utf-8 -*-
 import os
+import signal
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysqlapi.settings")
+import crane_ec2
+
+from mysqlapi.api import creator
+from mysqlapi.api.models import DatabaseManager, Instance
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "mysqlapi.settings"
+
+
+def huphandler(signum, frame):
+    creator.reset_queue()
+
+
+def termhandler(signum, frame):
+    creator.close_queue()
+
+
+def start():
+    client = crane_ec2.Client()
+    signal.signal(signal.SIGHUP, huphandler)
+    signal.signal(signal.SIGTERM, termhandler)
+    creator.set_model(Instance)
+    creator.build_queue()
+    creator.start_creator(DatabaseManager, client)
+
+start()
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
