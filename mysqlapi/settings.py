13dc95e73750e3f92e55311362a076f2e911fc59
Flavia Missi <flaviamissi@gmail.com>
2012-10-04 13:43:23 -0300

    removing path from wsgi file

--- mysqlapi/settings.py
+++ mysqlapi/settings.py
@@ -95,7 +95,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = "mysqlapi.urls"
 
 # Python dotted path to the WSGI application used by Django"s runserver.
-WSGI_APPLICATION = "mysqlapi.wsgi.application"
+WSGI_APPLICATION = "wsgi.application"
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
