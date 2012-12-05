2e15f55e8e7df99bc6f023f3fad64304edaffee9
Andrews Medina <andrewsmedina@gmail.com>
2012-12-05 17:45:22 -0200

    initial restore cmd

--- mysqlapi/api/management/commands/restore.py
+++ mysqlapi/api/management/commands/restore.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import NoArgsCommand
-from django.conf import settings
-
-from mysqlapi.api.database import export
 
 
 class Command(NoArgsCommand):
