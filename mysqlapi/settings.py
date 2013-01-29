93cf9f02ab1b63760ab8dbe647c6d710ad08d720
Francisco Souza <fss@corp.globo.com>
2013-01-29 13:43:38 -0200

    settings: disable some django garbage

--- mysqlapi/settings.py
+++ mysqlapi/settings.py
@@ -32,11 +32,11 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = True
+USE_L10N = False
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
