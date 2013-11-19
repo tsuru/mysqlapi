a16f01ad588755ee96558076bcdae17974449b60
Francisco Souza <fss@corp.globo.com>
2013-11-19 10:59:38 -0200

    settings: fix syntax
    Looks like I'm stupid.

--- mysqlapi/settings.py
+++ mysqlapi/settings.py
@@ -148,7 +148,7 @@ SHARED_SERVER_PUBLIC_HOST = os.environ.get(
 SHARED_USER = os.environ.get("MYSQLAPI_SHARED_USER", "root")
 SHARED_PASSWORD = os.environ.get("MYSQLAPI_SHARED_PASSWORD", "")
 
-USE_POOL = bool(os.environ.get("MYSQLAPI_USE_POOL"), False)
+USE_POOL = bool(os.environ.get("MYSQLAPI_USE_POOL", False))
 
 EC2_ENDPOINT = os.environ.get("MYSQLAPI_EC2_ENDPOINT")
 EC2_PORT = os.environ.get("MYSQLAPI_EC2_PORT")
